427be715c17a0dcd50a11aca3a7eeae6be2415fb
Joe Lowery <joseph.loweryit@gmail.com>
2020-11-10 00:00:43 -0500

    assets fixed

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+const burger = require("../models/burger");
+var connection = require("./connection");
+
+var orm = {
+  selectAll: function (table, cb) {
+    connection.query(`SELECT * FROM ??`, [table], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (table, burgerName, cb) {
+    connection.query(`INSERT INTO ?? (burger_name) VALUES (?)`, [table, burgerName], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function (table, id, cb) {
+    connection.query(`UPDATE ?? SET devoured = true
+    WHERE id = ?`, [table, id], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
